ef45e67402e13ade852d3aad38a44a6df49e05cf
SoundwaveEarthSpark <baranova.darya2002@gmail.com>
2024-08-26 20:16:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+done_homework=12
+time_spent=1.5
+kurs_name='Python'
+time_for_one_task= time_spent/done_homework
+print('Курс:',kurs_name,', всего задач:',done_homework,
+', затрачено часов:', time_spent, ', среднее время выполнения', time_for_one_task,'часа.')
